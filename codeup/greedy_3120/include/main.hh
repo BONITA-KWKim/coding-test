378c67a50ee0dbad3a30a34e0e211e406d5bd0a8
bonita <noberto.kyungwon@gmail.com>
2019-09-27 18:02:44 +0900

    First complete
    However, it is not right answer.

--- codeup/greedy_3120/include/main.hh
+++ codeup/greedy_3120/include/main.hh
@@ -2,15 +2,11 @@
 #define __MAIN_HH__
 
 #include <iostream>
-#include <cstdio>
-#include <string>
+//#include <cstdio>
 
-#define UP_1_DEGREE       1
-#define DOWN_1_DEGREE    -1
-#define UP_5_DEGREE       5
-#define DOWN_5_DEGREE    -5
-#define UP_10_DEGREE     10
-#define DOWN_10_DEGREE  -10
+#define BTN_DEGREE_1    1
+#define BTN_DEGREE_5    5
+#define BTN_DEGREE_10  10
 
 #define REMOTE_MAX_INPUT 6
 
--- codeup/greedy_3120/src/main.cc
+++ codeup/greedy_3120/src/main.cc
@@ -1,40 +1,76 @@
 #include "main.hh"
 
 int main(int argc, char **argv) {
-	std::cout << "Hello" << std::endl;
+	std::cout << "Greedy" << std::endl;
 
 	//std::string degree;
+	int degree[2];
 	char input[REMOTE_MAX_INPUT];
 	input[5] = ' ';
 
 	/* User input */
-	std::cin.getline(input, sizeof input);
-	std::cout << input << std::endl;
+	while (true) {
+		std::cin.getline(input, sizeof input);
+		std::cout << "input values: " << input << std::endl;
 
-	int i = 0;
-	int degree[2];
-	char* tok1 = strtok(input, " ");
-	while(tok1 != NULL){
-		std::cout<<tok1<<" count: "<<i<<std::endl;
+		char* tok1 = strtok(input, " ");
+		int i = 0;
+		while(tok1 != NULL){
+			degree[i] = (int)atoi(tok1);
+			tok1 = strtok(NULL," ");
+			++i;
+		}
 
-		degree[i] = (int)atoi(tok1);
-		tok1 = strtok(NULL," ");
-		++i;
+		/* user input error */
+		if (1 > degree[0] || 40 < degree[0] || 1 > degree[1] || 40 < degree[1]) {
+			std::cout << "input values have to be between 1 and 40" << std::endl;
+			continue;
+		} else {
+			break;
+		}
 	}
-
-	/* user input error */
 
 	/* greedy algorithm */
 	if (degree[1] > degree[0]) {
 		/* UP */
 		std::cout<< "UP" <<std::endl;
+
 	} else if (degree[1] < degree[0]) {
 		/* DOWN */
 		std::cout<< "DWON" <<std::endl;
+		int tmp = degree[1];
+		degree[1] = degree[0];
+		degree[0] = tmp;
 	} else {
 		/* No action is needed */
 		std::cout<< "NO ACTION" <<std::endl;
+
+		return 0;
 	}
+
+	std::cout<< degree[0] << " : " << degree[1] <<std::endl;
+
+	int count = 0;
+	while (true) {
+		int gap = degree[1] - degree[0];
+		if (BTN_DEGREE_10 <= gap) {
+			degree[0] += BTN_DEGREE_10;
+			++count;
+			std::cout<< "count: " << count << " push button: " << BTN_DEGREE_10 << "    " << degree[0] << ":" << degree[1] <<std::endl;
+		} else if (BTN_DEGREE_5 <= gap) {
+			degree[0] += BTN_DEGREE_5;
+			++count;
+			std::cout<< "count: " << count << " push button: " << BTN_DEGREE_5 << "     " << degree[0] << ":" << degree[1] <<std::endl;
+		} else if (BTN_DEGREE_1 <= gap) {
+			degree[0] += BTN_DEGREE_1;
+			++count;
+			std::cout<< "count: " << count << " push button: " << BTN_DEGREE_1 << "     " << degree[0] << ":" << degree[1] <<std::endl;
+		} else if (0 == gap) {
+			break;
+		}
+	}
+
+	std::cout << "Count: " << count << std::endl;
 
 	return 0;
 }
